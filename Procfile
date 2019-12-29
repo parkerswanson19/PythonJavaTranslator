ef3bfbb4bd8895fd437e869019df1685205ff9b4
Parker Swanson <parker@liquidspace.com>
2019-12-29 14:55:12 -0600

    new stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PythonJavaTranslator.wsgi
--- PythonJavaTranslator/settings.py
+++ PythonJavaTranslator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,4 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
