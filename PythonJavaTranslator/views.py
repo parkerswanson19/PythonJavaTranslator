8fd28585045281c706489a819a60070263af05de
Sourish Kundu <sourishkundu07@gmail.com>
2020-01-07 12:05:40 -0600

    Implemented the About the Creators Page! All links are functional

--- PythonJavaTranslator/views.py
+++ PythonJavaTranslator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import redirect
+from django.shortcuts import render
 
 
 def redirect_view(request):
     response = redirect('/translator/')
     return response
+
+
+def aboutTheCreators(request):
+    return render(request, 'aboutMe.html')
