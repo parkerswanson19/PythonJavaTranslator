020a3636f82942155cc97ccfb324a4d87e746d37
Sourish Kundu <sourishkundu07@gmail.com>
2020-01-22 15:02:17 -0600

    Number of words officially works now

--- RapAnalyzer/models.py
+++ RapAnalyzer/models.py
@@ -14,7 +14,7 @@ def find_lyrics(url):
     return lyrics
 
 
-def request_song_info(song_title, artist_name):
+def get_song_info(song_title, artist_name):
     base_url = 'https://api.genius.com'
     headers = {'Authorization': 'Bearer ' + 'ULuimckPtpbjfzBpV-nOi0UtSfmcaHtuUmz5v1w8hWmgUQphXNvglKhbDk_yjTz8'}
     search_url = base_url + '/search'
@@ -28,25 +28,25 @@ class Song:
     def __init__(self, name, artist):
         self.name = name
         self.artist = artist
-        self.lyrics = ""
+        self.lyrics = None
+        self.bare_lyrics = ""
+        self.num_of_words = 0
 
     def analyze(self):
-        # try:
-        name = self.name.split()
-        name = "-".join(name).replace(".", "")
-        artist = self.artist.split()
-        artist = "-".join(artist).replace(".", "")
+        song_info = get_song_info(self.name, self.artist)
+        song_info = song_info.json()
+        for hit in song_info["response"]["hits"]:
+            if hit["result"]["title"].lower().replace(".", "") == self.name.lower().replace(".", ""):
+                song = hit
+                self.lyrics = find_lyrics(song["result"]["url"])
+                break
+        else:
+            self.lyrics = "Error: Song not found. Check for typos."
 
-        url = f"https://genius.com/{artist}-{name}-lyrics"
-        self.lyrics = find_lyrics(url)
+        lines = self.lyrics.split("\n")
+        for line in lines:
+            if type(line) is None or len(line) == 0 or line[0] == "[":
+                continue
+            self.bare_lyrics += line + " "
 
-
-
-
-
-
-
-
-song = request_song_info('sicko mode', 'travis scott')
-json = song.json()
-print(json)
+        self.num_of_words = len(self.bare_lyrics.split())
