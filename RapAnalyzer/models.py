4c2b2db74e29ac7aadb19fc1936ad098634ed0a6
Sourish Kundu <sourishkundu07@gmail.com>
2020-01-24 10:37:58 -0600

    Finished indices and working on comments now

--- RapAnalyzer/models.py
+++ RapAnalyzer/models.py
@@ -71,27 +71,36 @@ class Song:
     jewelery = ["patek"]
 
     def __init__(self, name, artist, lyrics_query):
+        # Attributes that the user (can) enter
         self.name = name
-        self.full_name = ""
         self.artist = artist
         self.lyrics_query = lyrics_query
-        self.lyrics = None
-        self.bare_lyrics = ""
+
+        self.full_name = ""  # This is for the name of the song with the artist and any features
+        self.lyrics = ""  # This holds the original/full text representing the lyrics
+        self.bare_lyrics = ""  # String of all of the words extracted without any new lines or punctuations
+
+        # Stats about the song
         self.num_of_words = 0
         self.num_of_swear_words = 0
-        self.syllables = {}
+        self.num_of_lines = 0
+        self.num_of_syllables = 0
+        self.syllables_dict = {}  # Dictionary that holds the number of syllables of each word in the song
 
+        # The grade level indices
+        self.gunning_fog = 0
+        self.flesch = 0
+        self.power_sumner_kearl = 0
+
+    # Besides the original three attributes that the user inputs, this method calculates all of the other stats and
+    # indices
     def analyze(self):
-        if len(self.name) == 0 and len(self.artist) == 0:
+        # If the user only entered a lyric query, just return the first hit
+        if len(self.lyrics_query) > 0:
+            # Gets the json response from Genius with the entered lyric query
             song_info = get_song_info(self.lyrics_query)
-            # print(song_info["response"]["hits"])
-            # print(type(song_info["response"]["hits"]))
-            print(song_info["response"]["hits"][0]["result"]["url"])
-            print(type(song_info["response"]["hits"]))
             self.lyrics = find_lyrics(song_info["response"]["hits"][0]["result"]["url"])
             self.full_name = song_info["response"]["hits"][0]["result"]["full_title"]
-
-
         else:
             song_info = get_song_info(self.name, self.artist)
             # with open("file.json", 'w') as file:
@@ -122,8 +131,10 @@ class Song:
                 self.lyrics = "Error: Song not found. Check for typos."
 
         lines = self.lyrics.split("\n")
+        self.num_of_lines = len(lines)
         for line in lines:
             if type(line) is None or len(line) == 0 or line[0] == "[":
+                self.num_of_lines -= 1
                 continue
             self.bare_lyrics += line + " "
 
@@ -131,17 +142,70 @@ class Song:
         self.bare_lyrics = self.bare_lyrics.replace("(", "")
         self.bare_lyrics = self.bare_lyrics.replace(")", "")
         self.bare_lyrics = self.bare_lyrics.lower()
+        self.bare_lyrics = re.sub(r'[^a-zA-Z| ]', "", self.bare_lyrics)
+        # print(self.bare_lyrics)
         bare_lyrics_split = self.bare_lyrics.split()
-        bare_lyrics_split = set(bare_lyrics_split)
+        self.num_of_words = len(bare_lyrics_split)
+        Lyrics_set = set(bare_lyrics_split)
+
+        num_of_big_words = 0
+
+        for word in Lyrics_set:
+            self.syllables_dict[word] = find_syllables(word)
+                # greater than three syllables
 
         for word in bare_lyrics_split:
-            self.syllables[word] = find_syllables(word)
-
-        print(self.syllables)
-
-        print("Num of unique words: " + str(len(self.syllables.keys())))
-        self.num_of_words = len(bare_lyrics_split)
+            if self.syllables_dict[word] >= 3:
+                num_of_big_words += 1  # This is for the Gunning Fog index which needs the number of words that are
+            self.num_of_syllables += self.syllables_dict[word]
 
         for word in self.swear_words:
             self.num_of_swear_words += self.bare_lyrics.count(word)
 
+        # gunning fog index
+        avg_sen_len = self.num_of_words / self.num_of_lines
+
+        percentage_of_big_words = num_of_big_words / self.num_of_words
+        # print(num_of_big_words)
+        # print(self.num_of_words)
+        self.gunning_fog = (avg_sen_len + percentage_of_big_words) * 0.4
+        self.gunning_fog = float(f"{self.gunning_fog:.2f}")
+        # print(avg_sen_len)
+        # print(percentage_of_big_words)
+
+        # The Flesch Formula
+        x = self.num_of_words / self.num_of_lines * 1.015
+        print(f"num of words is {self.num_of_words} and number lines is {self.num_of_lines} and x is {x}")
+        y = self.num_of_syllables / self.num_of_words * 84.6
+        print(f"{self.num_of_syllables} y is {y}")
+        # print(self.num_of_syllabes)
+        level = 206.835 - (x + y)
+
+        if level <= 29:
+            self.flesch = 'Very difficult Post Graduate'
+        elif level <= 49:
+            self.flesch = 'Difficult College'
+        elif level <= 59:
+            self.flesch = 'Fairly Difficult High School'
+        elif level <= 69:
+            self.flesch = 'Standard 8th to 9th grade'
+        elif level <= 79:
+            self.flesch = 'Fairly Easy 7th grade'
+        elif level <= 89:
+            self.flesch = 'Easy 5th to 6th grade'
+        elif level <= 100:
+            self.flesch = 'Very Easy 4th to 5th grade'
+        else:
+            self.flesch = 'Below 5th grade lmao'
+
+
+        # Power Sumner Kearl
+        x = self.num_of_words / self.num_of_lines
+        # print("num of lines is: " + str(self.num_of_lines))
+        y = self.num_of_syllables
+        # print(f"x is{x} and y is {y}")
+        y /= (self.num_of_words / 100)
+        # print(f"x is{x} and y is {y}")
+        z = (x * 0.0778) + (y * 0.0455)
+        self.power_sumner_kearl = z - 2.2029
+        self.power_sumner_kearl = float(f"{self.power_sumner_kearl:.2f}")
