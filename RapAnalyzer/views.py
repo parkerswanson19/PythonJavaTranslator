040241c855cb2e807ea059335bcbdf03890e2eac
Sourish Kundu <sourishkundu07@gmail.com>
2020-02-04 00:37:40 -0600

    Added top lists in two categories

--- RapAnalyzer/views.py
+++ RapAnalyzer/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from .models import Song
-import time
+from .models import *
+
+
+# import time
 
 
 def RapAnalyzerHomePage(request):
-    return render(request, 'rap-analyzer.html')
+    top_lists = getTopLists()
+    print(top_lists)
+    return render(request, 'rap-analyzer.html', top_lists)
 
 
 def RapAnalyzerAnalyze(request):
+    dictionary = getTopLists()
     # t0 = time.clock()
     # Creates a new Song object and assigns it to a local variable
     # 'content' is the name of the first textbox on the HTML page. It's telling Django to take the content of that box,
@@ -20,8 +25,18 @@ def RapAnalyzerAnalyze(request):
 
     new_input.analyze()
 
+    dictionary['code'] = new_input
+
     # t1 = time.clock()
     # print("Time elapsed: ", t1 - t0)  # CPU seconds elapsed (floating point)
 
     # Re-renders the page and passes the InputtedCode object to the HTML file so the text boxes can be updated
-    return render(request, 'rap-analyzer2.html', {'code': new_input})
+    return render(request, 'rap-analyzer2.html', dictionary)
+
+
+def getTopLists():
+    top_lists = {
+        'adlibs_list': ["most ad libs", SongDB.objects.order_by('-adlibs')[:4]],
+        'swear_words_list': ["most swear words", SongDB.objects.order_by('-num_of_swear_words')[:4]]
+    }
+    return top_lists
